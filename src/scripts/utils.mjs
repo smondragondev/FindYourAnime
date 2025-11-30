ccd4900466f5add8df28d423027aed7b065d54f3
Sergio Mondragon <sergiomondragondev@gmail.com>
2025-11-30 01:14:04 -0500

    feat: add logo image and enhance header navigation; introduce project constants and update styles

--- src/scripts/utils.mjs
+++ src/scripts/utils.mjs
@@ -1,3 +1,5 @@
+import { PROJECT_NAME } from "./constants.mjs";
+
 export async function loadTemplate(path) {
   const res = await fetch(path);
   const template = await res.text();
@@ -11,6 +13,24 @@ export function renderWithTemplate(template, parentElement, data, callback) {
   }
 }
 
+export function getCurrentPath() {
+  const url = window.location.pathname;
+  return url;
+}
+
+export function addWayFinding() {
+  const navOptions = document.querySelectorAll("#nav-options li a");
+  navOptions.forEach(
+    (option) => {
+      const currentPath = getCurrentPath();
+      if (option.pathname === currentPath || option.pathname === "/index.html") {
+        option.classList.add("text-primary-900");
+        option.classList.add("border-t-3");
+      }
+    }
+  )
+}
+
 export async function loadHeaderFooter(callback) {
   const header = document.querySelector("header");
   const footer = document.querySelector("footer");
@@ -18,4 +38,5 @@ export async function loadHeaderFooter(callback) {
   const footerTemplate = await loadTemplate("./partials/footer.html");
   renderWithTemplate(headerTemplate, header, "", callback);
   renderWithTemplate(footerTemplate, footer);
+  addWayFinding();
 }
